e2cecb78431a938f38a258b12adeeab9071c83e6
ConstantTeen <kostet-1998@mail.ru>
2020-11-17 01:25:30 +0300

    bugs fixed

--- storages/index.py
+++ storages/index.py
@@ -5,7 +5,7 @@ import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        if not self.get_cookie("style"):
+        if not self.get_cookie("style-background"):
             self.set_cookie("style-background", "#f3b7b8")
 
         with open("{}/index.html".format(os.getcwd()), 'rb') as f:
